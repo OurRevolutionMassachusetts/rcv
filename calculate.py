b589a9c3011712c3ab926de301707783d67926d0
braiotta <braiotta@mit.edu>
2018-08-12 14:38:56 -0400

    rewritten to work in a first ballot/second ballot operation

--- calculate.py
+++ calculate.py
@@ -1,6 +1,6 @@
 from election.Election import Election
 
-source_file = 'ORMA gubernatorial endorsement (Responses) - Form Responses 1.csv'
+source_file = '2018_gub/Final Valid Gubernatorial Endorsement Responses.csv'
 
 # name of the columns holding the actual votes, and how to refer to them in results
 vote_cols = {
@@ -9,4 +9,36 @@ vote_cols = {
     'Select your preference for ORMA gubernatorial endorsement. [No Endorsement]':  'No Endorsement'
 }
 
+# create the Election object and feed it basic data
 e = Election(source_file=source_file, vote_cols=vote_cols)
+
+# load the file into a csv and reset relevant attrs
+e.bootstrap()
+
+# run the first ballot
+e.first_ballot()
+
+# run the second ballot
+# havnen't written this yet, but will go through all votes. for each one that had the knockout as first element in their
+# vote list(), will take the second element and add to results and then see where we're at percentage-wise
+# e.second_ballot()
+
+
+
+# first pass: only calculate top choice
+
+# anyone clear the post? done.
+# if not, knock out candidate with fewest first choice; recalc by:
+#   * taking everyone who voted for the loser as FIRST
+#   * treat their second choice as a first choice
+
+# anyone clear the post after looking at second choice? done.
+# if not, KIND OF AN OPEN QUESTION IF WE ONLY HAVE 3 OPTIONS
+# either
+#   * knock out next loser, what's left is winner
+#   * add third pref from people who voted for loser
+#   * add second pref from everyone who didn't vote for winner
+
+#e.first_ballot()
+#e.second_ballot()
+#e.third_ballot()
--- election/Election.py
+++ election/Election.py
@@ -1,6 +1,9 @@
 class Election:
-    def __init__(self, vote_cols=None, source_file=None, order=None, autoload=True):
+    def __init__(self, vote_cols=None, source_file=None, order=None, finish_line=60):
         self.data = None
+        self.ballots_run = []
+        self.finish_line = finish_line
+        self.knockouts = []
         self.order = order
         self.results = {}
         self.source_dir = './source/'
@@ -13,40 +16,57 @@ class Election:
         if self.order is None:
             self.order = ['First Preference', 'Second Preference', 'Third Preference']
 
-        if autoload and self.source_file and self.vote_cols:
-            self.load_data()
-            self.tabulate()
-
-    def load_data(self):
+    def bootstrap(self):
         import csv
-        self.votes = csv.DictReader(open(self.source_dir + self.source_file))
-
-    def rank(self):
-        for option, rankings in self.results.items():
-            shortname = self.vote_cols[option]
-            print('for option ' + shortname + ":")
-            for place in self.order:
-                percentage = (rankings[place] / self.vote_count) * 100
-                print("\t" + place + ': ' + str(rankings[place]) + ' (' + str(round(percentage, 2)) + '% of ' + str(self.vote_count) +
-                      ' votes cast)')
-            print()
-
-    def record_votes(self):
-        # extract the rankings for the different options
-        for vote in self.votes:
-            self.vote_count += 1
-            for col, shortname in self.vote_cols.items():
-                pref = vote[col]
-                if pref:
-                    if pref not in self.results[col]:
-                        self.results[col][pref] = 0
-                    self.results[col][pref] += 1
-
-    def tabulate(self):
-        # bootstrap the results from vote_cols
-        self.results = {k:{} for (k,v) in self.vote_cols.items()}
-        self.vote_count = 0
+        self.ballots_run = []
+        self.knockouts = []
+        self.data = list(csv.DictReader(open(self.source_dir + self.source_file)))
+        self.vote_count = len(self.data)
 
         self.record_votes()
-        # did the result meet the threshold
-        self.rank()
+
+    def first_ballot(self):
+        self.results = {v: 0 for (k, v) in self.vote_cols.items()}
+
+        # for the first ballot, we simply record everyone's first choice
+        for row in self.votes:
+            if row[0]:
+                self.results[row[0]] += 1
+
+        self.ballots_run.append('first')
+        self.report()
+
+    # for easy extraction, for each voter construct an array in order of preferences
+    def record_votes(self):
+        for row in self.data:
+            vote_dict = {}
+            vote = []
+
+            # create a key/value dict based on first pref/second pref/etc
+            for col, shortname in self.vote_cols.items():
+                choice = row[col]
+                if choice:
+                    vote_dict[choice] = shortname
+
+            # use that dict to construct a simple ordered list of the user's choices
+            for col in self.order:
+                if col in vote_dict:
+                    vote.append(vote_dict[col])
+                else:
+                    vote.append(None)
+
+            self.votes.append(vote)
+
+    def report(self):
+        ballot_name = self.ballots_run[-1] + ' ballot'
+        print(ballot_name)
+        print('--------')
+
+        for choice, raw in self.results.items():
+            percentage = round(((raw / self.vote_count) * 100), 2)
+
+            winner = ''
+            if percentage >= self.finish_line:
+                winner = ' ** WINNER **'
+
+            print(choice + ': ' + str(raw) + ' (' + str(percentage) + '% of ' + str(self.vote_count) + ')' + winner)
